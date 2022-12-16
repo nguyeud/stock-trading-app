86edb670c5a1fd59b1ec3d969e2ad3000363b1f0
Uyen Nguyen <nguyeud@gmail.com>
2022-12-16 14:09:35 -0500

    New logo color and added class to index.html <main>

--- src/scripts/toggle-theme.js
+++ src/scripts/toggle-theme.js
@@ -1,3 +1,4 @@
+const themeToggleBtn = document.getElementById('theme-toggle');
 const themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
 const themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
 const toggleNavLogo = document.getElementById("nav-logo");
@@ -10,39 +11,33 @@ if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localS
     themeToggleDarkIcon.classList.remove('hidden');
 }
 
-const themeToggleBtn = document.getElementById('theme-toggle');
-
 themeToggleBtn.addEventListener('click', function() {
 
-    // toggle icons inside button
+    // Toggle icons inside button
     themeToggleDarkIcon.classList.toggle('hidden');
     themeToggleLightIcon.classList.toggle('hidden');
 
-    // if set via local storage previously
+    // If set via local storage previously
     if (localStorage.getItem('color-theme')) {
         if (localStorage.getItem('color-theme') === 'light') {
             document.documentElement.classList.add('dark');
             localStorage.setItem('color-theme', 'dark');
-            toggleNavLogo.setAttribute("src", "../../public/images/logo.png");
             toggleBody.classList.add("bg-slate-900");
         } else {
             document.documentElement.classList.remove('dark');
             localStorage.setItem('color-theme', 'light');
-            toggleNavLogo.setAttribute("src", "../../public/images/logo-black.png");
             toggleBody.classList.remove("bg-slate-900");
         }
 
-    // if NOT set via local storage previously
+    // If NOT set via local storage previously
     } else {
         if (document.documentElement.classList.contains('dark')) {
             document.documentElement.classList.remove('dark');
             localStorage.setItem('color-theme', 'light');
-            toggleNavLogo.setAttribute("src", "../../public/images/logo-black.png");
             toggleBody.classList.remove("bg-slate-900");
         } else {
             document.documentElement.classList.add('dark');
             localStorage.setItem('color-theme', 'dark');
-            toggleNavLogo.setAttribute("src", "../../public/images/logo.png");
             toggleBody.classList.add("bg-slate-900");
         }
     }
