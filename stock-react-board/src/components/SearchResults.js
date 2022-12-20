945c2d0a68d645b975f0bc3398c4602b1e03028d
Nick Houghton <njhoughton5@gmail.com>
2022-12-20 12:33:01 -0800

    remove d/l button from react board

--- stock-react-board/src/components/SearchResults.js
+++ stock-react-board/src/components/SearchResults.js
@@ -6,7 +6,7 @@ const SearchResults = ({ results }) => {
   const { darkMode } = useContext(ThemeContext);
   const { setStockSymbol } = useContext(StockContext);
   return (
-    <ul
+    <ul id="listDrop"
       className={`absolute top-12 border-2 w-full rounded-md h-64 overflow-y-scroll ${
         darkMode
           ? "bg-gray-900 border-gray-800 custom-scrollbar custom-scrollbar-dark"
@@ -15,7 +15,7 @@ const SearchResults = ({ results }) => {
     >
       {results.map((item) => {
         return (
-          <li
+          <li 
             key={item.symbol}
             className={`cursor-pointer p-4 m-2 flex items-center justify-between rounded-md ${
               darkMode
--- stock-react-board/src/components/ThemeIcon.js
+++ stock-react-board/src/components/ThemeIcon.js
@@ -1,27 +1,54 @@
 import React, { useContext } from "react";
 import ThemeContext from "../context/ThemeContext";
 
-
 const ThemeIcon = () => {
-    const { darkMode, setDarkMode } = useContext(ThemeContext);
+  const { darkMode, setDarkMode } = useContext(ThemeContext);
+  document.getElementById("theme-toggle").addEventListener("click", () => {
+    setDarkMode(!darkMode);
+  });
+  
 
-    const toggleDarkMode = () => {
-        setDarkMode(!darkMode);
-    };
+  // document.getElementById("theme-toggle").addEventListener("click", () => {
+  //   setDarkMode(!darkMode);
+  // })
+  
 
-  return <button className={`rounded-lg border-neutral-400 p-1 relative right-8 xl:right:32 shadow-xl w-20 h-20 ${darkMode ? "shadow-yellow-100" : null
-}`} 
-  onClick={toggleDarkMode}
-  ><div className={`cursor-pointer stroke-1 ${
-    darkMode
-      ? "fill-yellow-400 stroke-yellow-800 shadow-yellow-800"
-      
-      : "fill-none stroke-neutral-400"
-  }`}>
-    <img src="https://media.istockphoto.com/id/1334613123/vector/moon-and-star-black-icon-of-moon-for-night-pictogram-of-crescent-and-star-logo-for-sleep-and.jpg?s=612x612&w=0&k=20&c=Gy6DGTiwY2lsGPXRr2f7kqPELZEUtG1MsSEce1BkPdo="
-        className="width:300px height:300px" alt="asdfd"/>
-        </div>
-  </button>
+  // window
+  // .matchMedia("(prefers-color-scheme: dark)")
+  // .addEventListener("change", () => {
+  //     setDarkMode(!darkMode)
+  // }) ;
+  // const toggleDarkMode = () => {
+  //   setDarkMode(!darkMode);
+  // };
+
+  // if (
+  //   window.matchMedia &&
+  //   window.matchMedia("(prefers-color-scheme: dark)").matches
+  // ) {
+  //   {
+  //     toggleDarkMode();
+  //   }
+  // }
+  // window
+  //   .matchMedia("(prefers-color-scheme: dark)")
+  //   .addEventListener("change", (event) => {
+  //     event.toggleDarkMode();
+  //   });
+
+  //   return <button className={`rounded-lg border-neutral-400 p-1 relative right-8 xl:right:32 shadow-xl w-20 h-20 ${darkMode ? "shadow-yellow-100" : null
+  // }`}
+  //   onClick={toggleDarkMode}
+  //   ><div className={`cursor-pointer stroke-1 ${
+  //     darkMode
+  //       ? "fill-yellow-400 stroke-yellow-800 shadow-yellow-800"
+
+  //       : "fill-none stroke-neutral-400"
+  //   }`}>
+  //     <img src="https://media.istockphoto.com/id/1334613123/vector/moon-and-star-black-icon-of-moon-for-night-pictogram-of-crescent-and-star-logo-for-sleep-and.jpg?s=612x612&w=0&k=20&c=Gy6DGTiwY2lsGPXRr2f7kqPELZEUtG1MsSEce1BkPdo="
+  //         className="width:300px height:300px" alt="asdfd"/>
+  //         </div>
+  //   </button>
 };
 
 export default ThemeIcon;
